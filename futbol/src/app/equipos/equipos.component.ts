e4a29c526e2abd564413ac09fa3c9a06ff3b3144
Alberto Garcia de Cortazar Rouret <mum@MacBook.local>
2019-03-25 19:53:24 +0100

    cambios visuales y formulario

--- futbol/src/app/equipos/equipos.component.ts
+++ futbol/src/app/equipos/equipos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Equipo } from './equipo';
 import es from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-equipos',
@@ -11,15 +12,14 @@ import { registerLocaleData } from '@angular/common';
 export class EquiposComponent implements OnInit {
 
   equipos: Array<Equipo>;
-  nuevoEquipo: Equipo;
   nombreEquipo: string;
   equipoSeleccionado: Equipo;
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     registerLocaleData( es );
-    this.nuevoEquipo = new Equipo('', '', 0, 0, '');
     this.equipos = [
       new Equipo('Athletic Club', 'Bilbao', 44171, 1898, 'https://cdn.icon-icons.com/icons2/1637/PNG/256/athletic-bilbao_109476.png'),
       new Equipo('Futbol Club Barcelona', 'Barcelona', 143459, 1899, 'https://cdn.icon-icons.com/icons2/1637/PNG/256/barcelona_109494.png'),
@@ -27,15 +27,26 @@ export class EquiposComponent implements OnInit {
       new Equipo('Real Club Celta de Vigo', 'Vigo', 22104, 1923, 'https://cdn.icon-icons.com/icons2/1637/PNG/256/celta-de-vigo_109482.png')
     ];
     this.nombreEquipo = '';
+    this.buildForm();
   }
 
   guardarEquipo() {
-    this.equipos.push(this.nuevoEquipo);
-    this.nuevoEquipo = new Equipo('', '', 0, 0, '');
+    this.equipos.push(this.formulario.value);
+    this.formulario.reset();
   }
 
   mostrarEquipo(equipo: Equipo): void {
     this.equipoSeleccionado = equipo;
+  }
+
+  buildForm() {
+    this.formulario = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      ciudad: '',
+      socios: '',
+      fundacion: '',
+      escudo: ''
+    });
   }
 
 }
